549fa9d5a584f98a5d320d30f88be4ab9f358c03
Hasan-Uz-Zaman <hasanashik100@gmail.com>
2024-05-20 20:28:02 +0600

    build game trailer

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -8,7 +8,7 @@ const useTrailers = (gameId: number) => {
     const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
     return useQuery({
         queryKey: ['trailers', gameId],
-        queryFn: apiClient.getAll
+        queryFn: apiClient.getAll,
     })
 }
 
